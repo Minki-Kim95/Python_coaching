e0db322cb0e95882ce3c4ad634279755867011a2
Minki-kim95 <rlaalsrl7777@gmail.com>
2019-09-27 01:20:53 +0900

    [EDIT] pylint style

--- week2/main2.py
+++ week2/main2.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request
+"""
+flask module
+"""
+from flask import Flask, render_template, request   # flask module
 
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
 @app.route('/')
 def test():
+    """
+    home of web
+    :return: hello.html
+    """
     return render_template('hello.html')
 
 @app.route('/post', methods=['POST'])
 def post():
+    """
+    post value of input data which name is test
+    :return: letter Hello world + input data
+    """
     value = request.form['test']
     return 'Hello world ' + value
 
