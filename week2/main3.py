67e68c02ec7e51747a042fb909212968d4bbcba5
Minki-kim95 <rlaalsrl7777@gmail.com>
2019-09-27 15:29:18 +0900

    [ADD] week2 assignment3

---
+++ week2/main3.py
@@ -0,0 +1,32 @@
+"""
+flask module
+"""
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    home of web
+    :return: meter_yard.html
+    """
+    return render_template('meter_yard.html')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    """
+    post value of input data which name is test
+    :return: transfered yard data (if data is string, reload index again)
+    """
+    try:
+        value = int(request.form['test'])
+        yard_value = str(value * 1.09361)
+        return yard_value + 'yard'
+
+    except ValueError:
+        return redirect(url_for('index')) # render_template('meter_yard.html')
+
+if __name__ == '__main__':
+    app.run()
